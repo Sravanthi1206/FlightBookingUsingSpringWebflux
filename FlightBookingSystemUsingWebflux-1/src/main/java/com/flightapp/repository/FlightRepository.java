1781363ee2a722d079d00ccecd723ce9dca20ff1
Sravanthi1206 <sravanthi.22bce20201@vitapstudent.ac.in>
2025-11-24 18:31:00 +0530

    added required repositories

--- FlightBookingSystemUsingWebflux-1/src/main/java/com/flightapp/repository/FlightRepository.java
+++ FlightBookingSystemUsingWebflux-1/src/main/java/com/flightapp/repository/FlightRepository.java
@@ -1,5 +1,10 @@
 package com.flightapp.repository;
 
-public interface FlightRepository {
+import com.flightapp.model.Flight;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
+public interface FlightRepository extends ReactiveMongoRepository<Flight, String> {
+    
+    Flux<Flight> findByOriginAndDestination(String origin, String destination);
 }
